8a76c4d53a5e251aabc13afbd343fc13117a8952
Dj X.O <41582063+xodj@users.noreply.github.com>
2021-06-29 15:23:26 +0300

    Update CMakeLists.txt
    All changes for Android build

--- CopyDependencies.cmake
+++ CopyDependencies.cmake
@@ -8,34 +8,45 @@ if(COPIED_DEPENDENCIES)
 endif()
 
 option(COPY_DEPENDENCIES "Automatically try copying all dependencies" ON)
+
 if(NOT COPY_DEPENDENCIES)
 	return()
 endif()
 
 #COPY DEPS
-message("\nCopy Dependencies...\n")
+message(STATUS "Copy Dependencies...")
 
 find_package(QT NAMES Qt6 Qt5 COMPONENTS Widgets Network REQUIRED)
 find_package(Qt${QT_VERSION_MAJOR} COMPONENTS Widgets Network REQUIRED)
 
 set(COPIED_DEPENDENCIES TRUE CACHE BOOL "Dependencies have been copied, set to false to copy again" FORCE)
 
-if(NOT CMAKE_BUILD_TYPE MATCHES "Debug")
-
+if(NOT CMAKE_BUILD_TYPE MATCHES "Debug" AND  MSVC)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Core.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Core.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Gui.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Gui.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Widgets.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Widgets.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Network.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Network.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/plugins/platforms/qwindows.dll ${PROJECT_BINARY_DIR}/Appfolder/Platforms/qwindows.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/plugins/styles/qwindowsvistastyle.dll ${PROJECT_BINARY_DIR}/Appfolder/Styles/qwindowsvistastyle.dll COPYONLY)
+endif()
 
-else(NOT CMAKE_BUILD_TYPE MATCHES "Debug")
-
+if(CMAKE_BUILD_TYPE MATCHES "Debug" AND MSVC)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Cored.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Cored.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Guid.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Guid.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Networkd.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Networkd.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Widgetsd.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Widgetsd.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/plugins/platforms/qwindowsd.dll ${PROJECT_BINARY_DIR}/Appfolder/Platforms/qwindowsd.dll COPYONLY)
 configure_file(${CMAKE_PREFIX_PATH}/plugins/styles/qwindowsvistastyled.dll ${PROJECT_BINARY_DIR}/Appfolder/Styles/qwindowsvistastyled.dll COPYONLY)
+endif()
 
-endif(NOT CMAKE_BUILD_TYPE MATCHES "Debug")
+if(MINGW)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Core.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Core.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Gui.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Gui.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Widgets.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Widgets.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/Qt${QT_VERSION_MAJOR}Network.dll ${PROJECT_BINARY_DIR}/Appfolder/Qt${QT_VERSION_MAJOR}Network.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/plugins/platforms/qwindows.dll ${PROJECT_BINARY_DIR}/Appfolder/Platforms/qwindows.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/plugins/styles/qwindowsvistastyle.dll ${PROJECT_BINARY_DIR}/Appfolder/Styles/qwindowsvistastyle.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/libstdc++-6.dll ${PROJECT_BINARY_DIR}/Appfolder/libstdc++-6.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/libwinpthread-1.dll ${PROJECT_BINARY_DIR}/Appfolder/libwinpthread-1.dll COPYONLY)
+    configure_file(${CMAKE_PREFIX_PATH}/bin/libgcc_s_seh-1.dll ${PROJECT_BINARY_DIR}/Appfolder/libgcc_s_seh-1.dll COPYONLY)
+endif(MINGW)
